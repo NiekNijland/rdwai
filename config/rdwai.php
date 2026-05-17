f181c564b68828fad12457a08ade7d0d37ed50e4
NiekNijland <niekjnijland@gmail.com>
2026-05-17 13:40:01 +0200

    Prepare for deploy

--- config/rdwai.php
+++ config/rdwai.php
@@ -7,7 +7,7 @@ return [
     'rdw_app_token' => env('RDW_APP_TOKEN'),
     'rate_limit' => [
         'per_minute' => env('RDWAI_RATE_LIMIT_PER_MINUTE', 10),
-        'per_day_global' => env('RDWAI_RATE_LIMIT_PER_DAY_GLOBAL', 1000),
+        'per_day_global' => env('RDWAI_RATE_LIMIT_PER_DAY_GLOBAL', 50),
         'feedback_per_minute' => env('RDWAI_RATE_LIMIT_FEEDBACK_PER_MINUTE', 30),
         'read_per_minute' => env('RDWAI_RATE_LIMIT_READ_PER_MINUTE', 60),
     ],
